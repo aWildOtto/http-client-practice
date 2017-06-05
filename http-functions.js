ee7671ae2a9b6cd54d1d3fe8befd40a95c6bd53d
otto hu <ottohu101@gmail.com>
2017-06-05 20:49:45 +0000

    step5

---
+++ http-functions.js
@@ -0,0 +1,19 @@
+var https = require("https");
+module.exports = function getHTML (options, callback) {
+
+  https.get(options, function (response) {
+    var dataStr = "";
+
+    response.setEncoding('utf8');
+
+    response.on('data', function (data) {
+      dataStr += data;
+    });
+
+    response.on('end', function() {
+      callback(dataStr);
+    });
+  });
+
+};
+
